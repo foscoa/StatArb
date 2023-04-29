2f3f24d7ea07b6e94559f7aa1cf008bc285d3142
foscoantognini <antognini.fosco@gmail.com>
2023-04-29 10:11:53 +0200

    first statarb algo

--- StatArb/main.py
+++ StatArb/main.py
@@ -1,16 +1,74 @@
-# This is a sample Python script.
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import statsmodels.api as sm
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import matplotlib.pyplot as plt
+
+def calc_zscore(series):
+    return (series - np.mean(series)) / np.std(series)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+### equities data - download and visualization -------------------------------------------------------------------------
+
+STOCK_A = "AAPL"
+STOCK_B = "MSFT"
+stock_a = yf.download(STOCK_A, start='2020-01-01', end='2023-04-28')
+stock_b = yf.download(STOCK_B, start='2020-01-01', end='2023-04-28')
+
+# dataframe with closing price
+closing_price = pd.concat([stock_a['Close'], stock_b['Close']], axis=1)
+closing_price.columns = [STOCK_A, STOCK_B]
+
+# show the closing price plot
+closing_price.plot.line()
+# plt.show()
+
+### --------------------------------------------------------------------------------------------------------------------
+
+### model - linear regression ------------------------------------------------------------------------------------------
+
+model = sm.OLS(closing_price[STOCK_A],
+               sm.add_constant(closing_price[STOCK_B])
+               ).fit()
+
+# get intercept and slope from model parameters
+intercept, slope = model.params
+
+# plot scatter plot of data
+plt.scatter(stock_b['Close'], closing_price[STOCK_A])
+
+# plot regression line
+plt.plot(closing_price[STOCK_B], intercept + slope * closing_price[STOCK_B], color='red')
+
+# set plot title and axis labels
+plt.title('Regression plot')
+plt.xlabel('b_close')
+plt.ylabel('a_close')
+
+# show the plot
+# plt.show()
+
+# Calculate the predicted values of A based on B using the fitted model
+a_pred = model.predict(sm.add_constant(closing_price[STOCK_B]))
+
+# Calculate the residuals between the predicted and actual values of A
+residuals = closing_price[STOCK_A] - a_pred
+
+# Calculate the Z-score of the residuals
+zscore = (residuals - residuals.mean()) / residuals.std()
+
+# Set the threshold for the Z-score
+threshold = 1.5
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+### --------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
