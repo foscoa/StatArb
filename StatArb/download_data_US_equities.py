d77e00107dd5463da6fa71ca6bf36ceb8b3c8953
Fosco <analytics@1lcapital.com>
2023-05-03 17:45:35 +0200

    created two functions: WriteCsvForEachTicker and WriteCsvForEachVariable, to write csv files

--- StatArb/download_data_US_equities.py
+++ StatArb/download_data_US_equities.py
@@ -4,6 +4,37 @@ import pandas as pd
 import yfinance as yf
 import plotly.express as px
 
+def WriteCsvForEachTicker(df, out_data_dir):
+
+    df = df.stack(level=0).rename_axis(['Date', 'Ticker']).reset_index(level=1)
+
+    # write one csv file with the data for each ticker
+    for i in tickerStrings:
+        single_df = df[df.Ticker == i]
+        single_df.to_csv(out_data_dir +
+                         i +
+                         '_' +
+                         str(single_df.index[0].date())
+                         + '_' + str(single_df.index[-1].date()) +
+                         '.csv',
+                         index=False)
+
+def WriteCsvForEachVariable(df, out_data_dir):
+
+    variables = list(df.stack(level=0).columns)
+
+    # write one csv file with the data for each variable
+    for i in variables:
+
+        single_df = df.stack(level=0)['Adj Close'].unstack(level=1)
+        single_df.to_csv(out_data_dir +
+                         i +
+                         '_' +
+                         str(single_df.index[0].date())
+                         + '_' + str(single_df.index[-1].date()) +
+                         '.csv',
+                         index=False)
+
 # upload tickers
 Tickers = pd.read_csv('/Users/foscoantognini/Documents/USEquityData/Tickers/nasdaq_screener_02052023.csv',
                       sep=',')
@@ -16,17 +47,8 @@ out_data_dir = '/Users/foscoantognini/Documents/USEquityData/Price_Volume_Dailie
 # stock_a = yf.download(my_ticker, start='2005-01-01', end='2023-04-28')
 
 tickerStrings = ['AAPL', 'MSFT']
-all_df = yf.download(tickerStrings, group_by='Ticker', start='2005-01-01', end='2023-04-28')
-all_df = all_df.stack(level=0).rename_axis(['Date', 'Ticker']).reset_index(level=1)
+df = yf.download(tickerStrings, group_by='Ticker', start='2005-01-01', end='2023-04-28')
 
-# write one csv file with the data for each ticker
-for i in tickerStrings:
-    single_df = all_df[all_df.Ticker == i]
-    single_df.to_csv(out_data_dir +
-                     i +
-                     '_' +
-                     str(single_df.index[0].date())
-                     + '_' + str(single_df.index[-1].date()) +
-                     '.csv',
-                     index=False)
+
+
 
