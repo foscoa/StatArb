501300311f5fa0ecdb796bd47b2313ab89964f9e
foscoantognini <antognini.fosco@gmail.com>
2023-05-07 00:31:54 +0200

    final script ready to download

--- StatArb/download_data_US_equities.py
+++ StatArb/download_data_US_equities.py
@@ -2,42 +2,60 @@
 
 import pandas as pd
 import yfinance as yf
-import json
+import random
 import plotly.express as px
+
+# directory where the files will be written
+data_dir = '/Users/foscoantognini/Documents/USEquityData/Price_Volume_Dailies/' # home
+# data_dir = 'C:\\Users\\Fosco\\Desktop\\Fosco\\USEquityData\\' # office
 
 # load tickers
 tick_dir = '/Users/foscoantognini/Documents/USEquityData/Tickers/nasdaq_screener_02052023.csv' # home
 # tick_dir = 'C:/Users/Fosco/Desktop/Fosco/Tickers/' # office
-Tickers = pd.read_csv(tick_dir, sep=',')
+Nasdaq_df = pd.read_csv(tick_dir, sep=',')
+all_tickers = list(Nasdaq_df.Symbol)
 
-# directory where the files will be written
-#  data_dir = '/Users/foscoantognini/Documents/USEquityData/Price_Volume_Dailies/' # home
-data_dir = 'C:\\Users\\Fosco\\Desktop\\Fosco\\USEquityData\\' # office
+# read files with downloaded data and store it in list
+already_downloaded_tickers = pd.read_csv(data_dir + '/downloaded_tickers.csv')
+already_downloaded_tickers = list(already_downloaded_tickers['0'])
 
-# download data from yahoo finance
-tickerStrings = ['MCD', 'KO']
-df = yf.download(tickerStrings, group_by='Ticker', start='2005-01-01', end='2023-04-28')
 
-variables = list(df.stack(level=0).columns)
+while len(already_downloaded_tickers) < len(all_tickers):
+    # remaining tickers
+    remaining_tickers = list(set(all_tickers) - set(already_downloaded_tickers))
 
-# write one csv file with the data for each variable
-for i in variables:
+    # select tickers to be downloaded
+    random_integer = random.randint(3, 6)
+    tickers_to_download = random.sample(remaining_tickers, 5)
 
-    # data with current variable
-    new_df = df.stack(level=0)[i].unstack(level=1)
+    # download data from yahoo finance
+    df = yf.download(tickers_to_download, group_by='Ticker', start='2005-01-01', end='2023-04-28')
 
-    # current variable name
-    data_string = data_dir + i + '_' + str(new_df.index[0].date()) + '_' + str(new_df.index[-1].date()) + '.csv'
+    variables = list(df.stack(level=0).columns)
 
-    # load data and reindexing
-    merged_df = pd.read_csv(data_string, index_col=0)
-    new_df.index = merged_df.index
+    # write one csv file with the data for each variable
+    for i in variables:
 
-    # Merge the two dataframes on their common column
-    merged_df = pd.merge(merged_df, new_df, on='Date')
+        # data with current variable
+        new_df = df.stack(level=0)[i].unstack(level=1)
 
-    # write merged df
-    merged_df.to_csv(data_string, index=True)
+        # current variable name
+        data_string = data_dir + i + '_' + str(new_df.index[0].date()) + '_' + str(new_df.index[-1].date()) + '.csv'
+
+        # load data and reindexing
+        merged_df = pd.read_csv(data_string, index_col=0)
+        new_df.index = merged_df.index
+
+        # Merge the two dataframes on their common column
+        merged_df = pd.merge(merged_df, new_df, on='Date')
+
+        # write merged df
+        merged_df.to_csv(data_string, index=True)
+
+
+    tickers_to_write = already_downloaded_tickers + tickers_to_download
+    pd.DataFrame(tickers_to_write).to_csv(data_dir + '/downloaded_tickers.csv', index=False)
+
 
 
 
