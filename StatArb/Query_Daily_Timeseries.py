18ee34e8f04f45ef404bf8560cfaeb26d0455a96
Fosco <analytics@1lcapital.com>
2023-05-18 20:01:52 +0200

    worked on time series generation

--- StatArb/Query_Daily_Timeseries.py
+++ StatArb/Query_Daily_Timeseries.py
@@ -2,6 +2,8 @@ import pymongo
 
 # Step 1 - Install and import pymongo
 import pymongo
+import pandas as pd
+import plotly.express as px
 
 # Step 2 - create a mongodb client, use default local host
 try:
@@ -12,9 +14,48 @@ except Exception:
 # select the collection
 collection = client.Financial_Data.Daily_Timeseries
 
-quer_result = collection.find({"Data.Symbol": "AAPL"})
+def generateTimeSeries(symbol, variable, collection):
 
-print(list(quer_result))
+    # Specify the symbols you want to retrieve the time series for
+    symbols = ['CVX', 'STZ']
+
+    # Define the aggregation pipeline
+    pipeline = [
+        {'$match': {'Data.Symbol': {'$in': symbols}}},
+        {'$unwind': '$Data'},
+        {'$match': {'Data.Symbol': {'$in': symbols}}},
+        {'$group': {'_id': '$timestamp', 'Data': {'$push': '$Data'}}},
+        {'$project': {'_id': 0, 'timestamp': '$_id', 'Data': 1}}
+    ]
+
+    # Execute the aggregation pipeline
+    results = collection.aggregate(pipeline)
+
+    # Extract the data for each symbol and create a Pandas DataFrame
+    data = {}
+    for symbol in symbols:
+        data[symbol] = []
+
+    for result in results:
+        timestamp = result['timestamp']
+        for entry in result['Data']:
+            if entry['Symbol'] in symbols:
+                data[entry['Symbol']].append((timestamp, entry['Adj Close']))
+
+    # Create a Pandas DataFrame from the extracted data
+    df = pd.DataFrame(data)
+
+    # Set the timestamp as the index
+    df.set_index('timestamp', inplace=True)
+
+    # Print the resulting time series
+    return df
+
+cvx = generateTimeSeries('CVX')
+
+# plot cumulative pnl
+fig = px.line(cvx)
+fig.show()
 
 
 
--- StatArb/dowload_yahoo_US_equity_MongoDB.py
+++ StatArb/dowload_yahoo_US_equity_MongoDB.py
@@ -1,7 +1,9 @@
+
 import pandas as pd
 import yfinance as yf
 import random
 import pymongo
+import numpy as np
 import time
 import datetime as dt
 import plotly.express as px
@@ -20,7 +22,7 @@ def yf_to_JSON(df):
 
     return json_dict
 
-def updateDaily_Timeseries(json_new):
+def update_Daily_Timeseries(json_new, collection):
     # updating database
     for i in json_new:
         # check if timeseries already exist
@@ -40,11 +42,19 @@ def updateDaily_Timeseries(json_new):
             if len(set1.intersection(set2)) > 0:
                 print("At node " + new_document['_id'] + " there is a duplicate: " +
                       str(set1.intersection(set2)))
-                return
-            else:
-                new_document['Data'] = new_document['Data'] + i['Data']
-                collection.find_one_and_replace(filter={'timestamp': i['timestamp']},
-                                                replacement=new_document)
+
+            new_document['Data'] = new_document['Data'] + i['Data']
+            collection.find_one_and_replace(filter={'timestamp': i['timestamp']},
+                                            replacement=new_document)
+
+def update_Nasdaq_Data(last_nasdaq_data, collection):
+    new_symbols = np.setdiff1d(last_nasdaq_data.Symbol, collection.distinct('Symbol'), assume_unique=True)
+
+    if len(new_symbols) == 1 & np.isnan(new_symbols[0]):
+        print('No new symbols to add in Nasdaq_Data')
+    else:
+        # TODO add new symbols
+        print()
 
 
 # Create a mongodb client, use default local host
@@ -57,46 +67,49 @@ except Exception:
 # Ticker list from https://www.nasdaq.com/market-activity/stocks/screener. Filter for NOT nano-cap
 
 # load tickers
-tick_dir = '/Users/foscoantognini/Documents/USEquityData/Tickers/nasdaq_screener_02052023.csv' # home
-# tick_dir = 'C:/Users/Fosco/Desktop/Fosco/Tickers/' # office
-# Nasdaq_df = pd.read_csv(tick_dir, sep=',')
-# all_tickers = list(Nasdaq_df.Symbol)
+# tick_dir = '/Users/foscoantognini/Documents/USEquityData/Tickers/'  # home
+tick_dir = 'C:\\Users\\Fosco\\Desktop\\Fosco\\USEquityData\\' # office
+tick_file_names = 'nasdaq_screener_1684394200429.csv'
+last_nasdaq_data = pd.read_csv(tick_dir + tick_file_names)
+
+# collection_NDAQ.insert_many(last_nasdaq_data.to_dict(orient='records')) #only to be used in the first time
 
 
-# select the collection
-collection = client.Financial_Data.Daily_Timeseries
-
-# print unique symbols in the database
-db_symbols = collection.distinct('Data.Symbol')
-
-print("There are " +
-      str(len(db_symbols)) +
-      " symbols in the Daily_Timeseries database: " +
-      str(db_symbols))
+# select Nasdaq_Data collection
+collection_NDAQ = client.Financial_Data.Nasdaq_Data
 
 # Start time series
-start_date = '2023-04-01'
-end_date = '2023-04-28'
+start_date = '2001-01-01'
+end_date = '2023-05-01'
 
+# select Daily_Timesieries collection
+collection_DT = client.Financial_Data.Daily_Timeseries
+
+# print unique symbols in the database
+DT_symbols = set(collection_DT.distinct('Data.Symbol'))
+
+print("There are " +
+      str(len(DT_symbols)) +
+      " symbols in the Daily_Timeseries database: " +
+      str(DT_symbols))
+
+NDAQ_symbols = set(collection_NDAQ.distinct('Symbol'))
+
+remaining_tickers = list(np.setdiff1d(NDAQ_symbols, DT_symbols, assume_unique=True)[0])
 
 # select tickers to be downloaded
 random_integer = random.randint(2, 7)
-# tickers_to_download = random.sample(remaining_tickers, random_integer)
-tickers_to_download = ["AAPL", "MSFT"]
+tickers_to_download = random.sample(remaining_tickers, random_integer)
 
 # download data from yahoo finance
-df1 = yf.download(["KO", "AMZN"], group_by='Ticker', start='2023-05-01', end='2023-05-08')
-df_new_date = yf.download(["KO", "AMZN"], group_by='Ticker', start='2023-04-01', end='2023-04-08')
-df_new_tck = yf.download(["AAPL", "MSFT"], group_by='Ticker', start='2023-05-01', end='2023-05-08')
+temp_yf = yf.download(tickers_to_download,
+                      group_by='Ticker',
+                      start=start_date,
+                      end=end_date)
 
-json1 = yf_to_JSON(df1)
-json_new_date = yf_to_JSON(df_new_date)
-json_new_tck = yf_to_JSON(df_new_tck)
+json_yf = yf_to_JSON(temp_yf)
 
-
-updateDaily_Timeseries(json1)
-updateDaily_Timeseries(json_new_date)
-updateDaily_Timeseries(json_new_tck)
+update_Daily_Timeseries(json_yf, collection_DT)
 
 
 
