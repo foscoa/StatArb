b3a2fe13567f3235b7838019e826f5e51cd3ea88
foscoantognini <antognini.fosco@gmail.com>
2023-06-11 22:07:03 +0200

    worked on risk-free

--- StatArb/dowload_risk_free.py
+++ StatArb/dowload_risk_free.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pymongo
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
 
 # Create a mongodb client, use default local host
@@ -10,17 +11,26 @@ try:
 except Exception:
     print("Error: " + Exception)
 
+# select the collection
+collection = client.Financial_Data.Risk_Free
+
 # https://markets.newyorkfed.org/static/docs/markets-api.html#/Reference%20Rates
 json_rf = pd.read_json("https://markets.newyorkfed.org/api/rates/all/search.json?startDate=2001-01-01&endDate=2023-05-01")
 
 rf_dates = [item['effectiveDate'] for item in json_rf.refRates]
 rf_dates = np.unique(rf_dates)
 
-json_rf_to_write = {}
-
-for j in rf_dates:
-    temp_dict = {}
+for date in rf_dates:
+    temp_list = []
     for k in json_rf.refRates:
-        dict((key, k[key]) for key in ['type', 'percentRate'])
+        # filet for SOFR or EFFR:
+        if ('SOFR' in k.values()) or ('EFFR' in k.values()):
+            temp_list.append(dict((key, k[key]) for key in ['type', 'percentRate']))
+
+    date_obj = datetime.strptime(date, '%Y-%m-%d')
+    iso_date = date_obj.isoformat()
+    collection.insert_one({iso_date:temp_list})
+
+
 
 
